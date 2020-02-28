b8ac50bcbb879da6cb784e4f2de09299a0284d11
tomo <tnakagaw22@hotmail.com>
2020-02-27 21:45:49 -0500

    read path from config

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import yfinance as yf
 import os.path
+import json
 
 from ticker_simpol import get_sp500_symbol
 
-save_path = '/home/tomo/dev/stock/data/histories'
+with open('key.json') as config:
+    config_data = json.load(config)
+    save_path = config_data['histories-saving-directory']
+
 sp500_symbols = get_sp500_symbol()
 
 for symbol in sp500_symbols:
